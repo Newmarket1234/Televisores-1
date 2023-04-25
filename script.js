a880e46241e36158fc07aa311e65b81b318bb47e
GALERIASHYPERMARKET <131471749+GALERIASHYPERMARKET@users.noreply.github.com>
2023-04-25 09:31:29 -0400

    3 files modified, 1 added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+function my_function(element){
+ var text=element.textContent;
+alert("hola");
+var length = 18;
+var trimmedString = text.length > length ? 
+                    text.substring(0, length - 3) + "..." : 
+                    text;
+element.textContent = trimmedString;
+}
